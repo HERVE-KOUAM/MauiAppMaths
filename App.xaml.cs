4dd0c581460b478f68f0e02a039cfbbf428e6eaf
KOUAM KAMDEM HERVE <hkouamherve145@gmail.com>
2024-11-08 15:07:49 -0500

    push initial

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMaths
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMaths
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
